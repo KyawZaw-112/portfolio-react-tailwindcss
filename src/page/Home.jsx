7154b11335207c1238c1b9fea802408208a4dbe0
KyawZaw-112 <winkyawzaw660@gmail.com>
2025-04-29 01:27:44 +0700

    fix: home page responsive for xl size

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
             style={{
                 lineHeight: "25px"
             }}
-            className={"w-1/3 md:w-1/4  dark:text-white dark-bg-gray-700 absolute right-0 md:px-3 bottom-0 font-share font-black text-sm"}>
+            className={"w-1/3 md:w-1/4 xl:w-1/6  dark:text-white dark-bg-gray-700 absolute right-0 md:px-3 bottom-0 font-share font-black text-sm"}>
           Born in 2001, <br/> in Yangon, Myanmar. <br/> I believe <br/> web design <br/> can be more <br/> diverse and inspiring. <br/> With a mission <br/> to present <br/> the possibilities <br/> of web design, <br/> I am pursuing <br/> new expressions <br/> through experiments <br/> and thought.
         </div>
     );
