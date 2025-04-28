8ad619f5d491931f77b2e995d70e8f4a268b34d8
KyawZaw-112 <winkyawzaw660@gmail.com>
2025-04-29 01:36:14 +0700

    change: contact notion link change

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -3,11 +3,11 @@ import {MdOutlineArrowOutward} from "react-icons/md";
 const Contact = () => {
     return (
         <div className={"absolute right-2 bottom-2"}>
-            <a className={"flex gap-1 items-center  hover:text-gray-500 dark:hover:text-gray-800 transition-all"} target={"_blank"} href="https://www.notion.so/Self-Introduction-1e3cd129fdb780708049fb9e32fd3ef1?pvs=4">
-                <p className={" dark:text-white font-raleway font-black text-xs tracking-widest "}>
+            <a className={"flex gap-1 items-center"} target={"_blank"} href="https://pale-begonia-993.notion.site/Self-Introduction-1e3cd129fdb780708049fb9e32fd3ef1">
+                <p className={" dark:text-white font-raleway   hover:text-gray-500 dark:hover:text-gray-800 transition-all font-black text-xs tracking-widest "}>
                     About me
                 </p>
-                <p className={" dark:text-white font-raleway font-black text-xs tracking-widest"}>
+                <p className={" dark:text-white font-raleway   hover:text-gray-500 dark:hover:text-gray-800 transition-all font-black text-xs tracking-widest"}>
                     <MdOutlineArrowOutward />
                 </p>
             </a>
