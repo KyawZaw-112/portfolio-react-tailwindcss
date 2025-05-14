b3a3b1c75a9d0d351b525a5a05f9aad1876b54ab
KyawZaw-112 <winkyawzaw660@gmail.com>
2025-05-14 20:01:49 +0700

    Add : download functionality

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import {MdOutlineArrowOutward} from "react-icons/md";
 const Contact = () => {
     return (
-        <div className={"absolute right-2 bottom-2"}>
+        <div className={"absolute flex flex-col gap-4 right-2 bottom-2"}>
+            <a className={"flex gap-1 items-center"} target={"_blank"} href="cv-1.pdf">
+                <p className={" dark:text-white font-raleway   hover:text-gray-500 dark:hover:text-gray-800 transition-all font-black text-xs tracking-widest "}>
+                    Download CV
+                </p>
+                <p className={" dark:text-white font-raleway   hover:text-gray-500 dark:hover:text-gray-800 transition-all font-black text-xs tracking-widest"}>
+                    <MdOutlineArrowOutward />
+                </p>
+            </a>
             <a className={"flex gap-1 items-center"} target={"_blank"} href="https://pale-begonia-993.notion.site/Self-Introduction-1e3cd129fdb780708049fb9e32fd3ef1">
                 <p className={" dark:text-white font-raleway   hover:text-gray-500 dark:hover:text-gray-800 transition-all font-black text-xs tracking-widest "}>
                     About me
